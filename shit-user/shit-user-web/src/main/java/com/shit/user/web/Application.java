1c75f0bcc879ec23b3d6c70f585481bc05ae2cae
Long <jyl0401@163.com>
2017-11-23 15:13:05 +0800

    springboot demo for shit-user module.
    Signed-off-by: Long <jyl0401@163.com>

---
+++ shit-user/shit-user-web/src/main/java/com/shit/user/web/Application.java
@@ -0,0 +1,18 @@
+package com.shit.user.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration  
+@ComponentScan
+@EnableAutoConfiguration
+@MapperScan("com.shit.user.dao.mapper")
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
