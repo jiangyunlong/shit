e3d110b262319f00707897cca3819fd903f08e92
Long <jyl0401@163.com>
2017-12-06 10:28:57 +0800

    add logback demo.
    Signed-off-by: Long <jyl0401@163.com>

---
+++ shit-user/shit-user-web/src/main/java/com/shit/user/web/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.shit.user.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @TODO
+ * @author Long
+ * @date 2017年12月6日上午10:23:03
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
